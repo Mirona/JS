814e41ce040e7a96a212ba104b9f8c33845f5124
Mirona Constantinescu <mirona.see@gmail.com>
2015-05-27 19:41:38 -0700

    Number Function

--- TypeConversion.js
+++ TypeConversion.js
@@ -1,4 +1,11 @@
 var age = prompt('Enter Age', '');
 alert'You are' + (Number(age) + 1 + 'Years Old';
 
+//Number Function
+
+var currentYear = 2015;
+var vehicleYear = prompt('What is the year of your vehicle ?')
+var age = currentYear - Number(vehicleYear);
+alert('Your vehicle is' + age + 'Years old')
+
 
