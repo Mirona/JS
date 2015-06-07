95daa5b1fd384cb1e38b9ed40740a52f89a62e1a
Mirona Constantinescu <mirona.see@gmail.com>
2015-06-07 11:57:08 -0700

    Value vs NoValue

---
+++ value_vs_novalue.js
@@ -0,0 +1,9 @@
+//determining whether a variable has a value. If keyword evaluates true if variable has a value or false if variable is either undefined or null. 
+
+if (myVar){
+	alert('myVar has a value');
+}
+
+else {
+	alert('myVar does not have a value')
+}
