a95a3577355c007ecd0630341b00fd4cac81f4aa
Mirona Constantinescu <mirona.see@gmail.com>
2015-06-07 12:28:17 -0700

    change1

--- value_vs_novalue.js
+++ value_vs_novalue.js
@@ -1,9 +1,12 @@
 //determining whether a variable has a value. If keyword evaluates true if variable has a value or false if variable is either undefined or null. 
 
-if (myVar){
-	alert('myVar has a value');
-}
 
-else {
-	alert('myVar does not have a value')
+var myVar=[null,0,''];
+for (i=0;i<myVar.length; i++)
+{
+ if(myVar[i])
+    alert('myVar has a value ' + myVar[i]);
+
+ else 
+	alert('myVar does not have a value ' + myVar[i]);
 }
