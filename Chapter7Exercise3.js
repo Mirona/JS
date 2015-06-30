1bad1a280c0c281560c37afbb0e07b3c52b00ca3
Mirona Constantinescu <mirona.see@gmail.com>
2015-06-30 15:03:11 -0700

    Chapter 7 Exercise 3

---
+++ Chapter7Exercise3.js
@@ -0,0 +1,11 @@
+//Chapter 7 Exercise 3
+function addNumbers() {
+firstNum = 4;
+secondNum = 8;
+result = firstNum + secondNum;
+return result;
+}
+result = 0;
+alert(result);
+result = addNumbers();
+alert(result);
